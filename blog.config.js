a97bb1232e737b6b543787fb1b1c5fe5bc59844c
Ideal Candys <155033919+IdealCandys@users.noreply.github.com>
2023-12-28 19:44:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '尚意食品知识库', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '若图文内容未更新，请手动刷新网页两次', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.idealcands.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.idealcandys.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'IdealCandys', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
